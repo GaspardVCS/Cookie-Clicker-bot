b81c0e3fd1aa67979d4606a2d7cbabfd26f437b9
Gaspard <gaspard.vingtrinier@student-cs.fr>
2020-10-02 09:37:17 +0200

    test

--- test.py
+++ test.py
@@ -1,8 +1,6 @@
-        menu = self.driver.find_element_by_id("menu")
-        a = menu.find_elements_by_tag_name('a')
-        a[16].click() #On clique sur Short Numbers ON
-        menu.find_elements_by_tag_name('div')[0].click() #On ferme la fenetre d'option
         
-        
-        # Contiendra le lien de la partie une fois enregistrée
-        self.link = ''
+    def click(self):
+        Cookie = self.driver.find_element_by_id("bigCookie")
+        Cookie.click()
+    
+    
